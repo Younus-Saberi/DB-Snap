c5ace5040a1508ccddf290d183cc53324fa26612
Younus-Saberi <younussaberi@gmail.com>
2025-02-24 21:22:30 +0530

    initial commit

---
+++ src/test/java/com/example/dbsnap/DbSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbsnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSnapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
